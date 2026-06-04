c8f6e84c0c9d81a3d807fe23bf8cf5279d2efcd8
uoahns23 <uoow.23@gmail.com>
2026-06-05 03:08:11 +0900

    feat/redirect와 callback으로 코드 받기

--- src/main/java/com/sooyeon/kakaologintest/auth/KakaoController.java
+++ src/main/java/com/sooyeon/kakaologintest/auth/KakaoController.java
@@ -1,13 +1,15 @@
 package com.sooyeon.kakaologintest.auth;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-@RestController
+@Controller
 public class KakaoController {
 
     @Value("${kakao.client-id}")
@@ -20,9 +22,17 @@ public class KakaoController {
     public String kakaoLogin() {
         String encodedRedirectUri = URLEncoder.encode(redirectUri, StandardCharsets.UTF_8);
 
-        return "https://kauth.kakao.com/oauth/authorize"
+        String kakaoLoginUrl = "https://kauth.kakao.com/oauth/authorize"
                 + "?response_type=code"
                 + "&client_id=" + clientId
                 + "&redirect_uri=" + encodedRedirectUri;
+
+        return "redirect:" + kakaoLoginUrl;
+    }
+
+    @ResponseBody
+    @GetMapping("/callback")
+    public String kakaoCallback(String code) {
+        return "카카오 인가 코드: " + code;
     }
 }
